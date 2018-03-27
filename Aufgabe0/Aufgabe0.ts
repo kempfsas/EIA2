a928a27a76fe505bd91cca06a585e195ae5c2fc4
kempfsas <37650925+kempfsas@users.noreply.github.com>
2018-03-27 13:06:49 +0200

    Verbesserung

--- Aufgabe0/Aufgabe0.ts
+++ Aufgabe0/Aufgabe0.ts
@@ -1,6 +1,6 @@
 
-function Willkommen() {
-    var name = prompt("Enter your name", "");
+function Willkommen() : void {
+    var name : string = prompt("Enter your name", "");
     if (name != null) {
         document.getElementById("Typ").innerHTML =
             "Hallo " + name + "! Willkommen auf der Mini-Website.";
